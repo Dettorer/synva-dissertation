1e390823da52c3bc7ed0af416ce4fb9571dea51e
Paul Dettorer Hervot <dettorer@dettorer.net>
2022-02-02 14:53:54 +0100

    template: use the kaobook class

--- .latexmkrc
+++ .latexmkrc
@@ -1,5 +1,8 @@
+# By default, build a PDF file (using XeLaTeX) in the `build` folder from `main.tex`
 $pdf_mode = 5;
+$out_dir = 'build';
+@default_files = ('./main.tex');
 $pdf_previewer = 'start evince';
 
-@default_files = ('main.tex');
-$out_dir = 'build';
+# template (custom .cls and .sty)
+ensure_path( 'TEXINPUTS', './template/kaobook-0.9.7//' );
--- flake.nix
+++ flake.nix
@@ -8,27 +8,55 @@
 
   outputs = { self, nixpkgs, utils }:
   (utils.lib.eachSystem utils.lib.allSystems (system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-    texDistribution = pkgs.texlive.combine {
-      inherit (pkgs.texlive) scheme-small latex-bin latexmk;
-    };
-  in
-  rec {
-    packages.dissertation = pkgs.stdenv.mkDerivation {
-      name = "PaulHervotSYNVADissertation";
-      src = ./.;
-      buildInputs = [ texDistribution ];
-      phases = ["unpackPhase" "buildPhase"];
-      buildPhase = ''
-          latexmk -interaction=nonstopmode -outdir=$out
-      '';
-    };
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      texDistribution = pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          # base scheme
+          scheme-small
 
-    defaultPackage = packages.dissertation;
+          # template packages
+          xifthen ifmtarg options xpatch xstring imakeidx hyphenat needspace
+          placeins marginnote marginfix sidenotes changepage chngcntr footmisc
+          footnotebackref tikzpagenodes ifoddpage multirow floatrow etoc
+          morewrites enumitem mdframed zref subfiles todonotes algorithm2e relsize
+          ccicons glossaries mfirstuc xfor datatool nomencl abstract csquotes
+          blindtext thmtools bigfoot biblatex cleveref
 
-    devShell = pkgs.mkShell {
-      buildInputs = [ texDistribution ];
-    };
-  }));
+          # utilities
+          latex-bin latexmk biber;
+      };
+      FONTCONFIG_FILE = pkgs.makeFontsConf {
+        fontDirectories = with pkgs; [ libertinus liberation_ttf ];
+      };
+    in
+    rec {
+      packages.dissertation = pkgs.stdenv.mkDerivation {
+        name = "PaulHervotSYNVADissertation";
+        src = ./.;
+        buildInputs = [ pkgs.coreutils texDistribution pkgs.glibcLocales ];
+        phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+        buildPhase = ''
+          export FONTCONFIG_FILE=${FONTCONFIG_FILE}
+          mkdir -p .cache/texmf-var
+          DIR=$(mktemp -d)
+          env \
+            TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+            SOURCE_DATE_EPOCHE=${toString self.lastModified} \
+            latexmk -interaction=nonstopmode
+        '';
+        installPhase = ''
+          mkdir -p $out
+          mv build/main.pdf $out/Paul_Hervot_M2_dissertation.pdf
+        '';
+      };
+
+      defaultPackage = packages.dissertation;
+
+      devShell = pkgs.mkShell {
+        inherit FONTCONFIG_FILE;
+        buildInputs = [ texDistribution pkgs.glibcLocales pkgs.biber pkgs.evince ];
+      };
+    }
+  ));
 }
